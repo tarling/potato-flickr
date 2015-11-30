b889671de1abce71250ee475b3463941a6f6fa81
James Tarling <james.tarling@gmail.com>
2015-11-30 20:38:21 +0000

    Caching data in service Layout changes

--- src/js/data-service.js
+++ src/js/data-service.js
@@ -1,20 +1,29 @@
 define(['./app'],function(app){
 	
-	app.factory("dataService", function($http, $sce){
-      return {
-        getData:function(){
-          return $http.jsonp('https://api.flickr.com/services/feeds/photos_public.gne?tags=potato&tagmode=all&format=json&jsoncallback=JSON_CALLBACK')
-            .then(function(response) {
-              return response.data.items.map(function(item){
-                //convert name to Date object so we can format it in the view
-                item.date_taken = new Date(item.date_taken);
-                item.description = $sce.trustAsHtml(item.description);
-                item.tags = item.tags.split(" ");
-                return item;
-              });
-          });
+	app.factory("dataService", function($http, $sce, $q, $timeout){
+        var items;
+        return {
+            getData:function(){
+                if (items)
+                {
+                    var deferred = $q.defer();
+                    deferred.resolve(items);
+                    return deferred.promise;
+                } else {
+                    return $http.jsonp('https://api.flickr.com/services/feeds/photos_public.gne?tags=potato&tagmode=all&format=json&jsoncallback=JSON_CALLBACK')
+                        .then(function(response) {
+                        items = response.data.items.map(function(item){
+                            //convert name to Date object so we can format it in the view
+                            item.date_taken = new Date(item.date_taken);
+                            item.description = $sce.trustAsHtml(item.description);
+                            item.tags = item.tags.split(" ");
+                            return item;
+                        });
+                        return items;
+                    });
+                }
+            }
         }
-      }
     });
 	
 });
