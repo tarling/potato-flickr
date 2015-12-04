a5938a9fa78c1911fa27adf3c1a59e5e2e2aac12
James Tarling <james.tarling@gmail.com>
2015-12-04 15:59:41 +0000

    Gulp sync fixes

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ var gulp = require('gulp'),
     htmlreplace = require('gulp-html-replace'),
     minifyHTML = require('gulp-minify-html'),
     minifyCss = require('gulp-minify-css'),
-    del = require('del');
+    del = require('del'),
+    runSequence = require('run-sequence');
 
 var src = "./src/";
 var devPath = "./dev/";
@@ -16,24 +17,25 @@ var copyOptions = {
   base: src
 }
 
-gulp.task('default', [
-  'set-deploy-path'
-  ,'clean'
-  , 'rjs'
-  , 'convert-html'
-  , 'copy-partials'
-  , 'compress-css'
-  , 'copy-ie-js',
-], function() {});
+gulp.task('default', function(callback) {
+  runSequence('set-deploy-path',
+            'clean',
+            ['rjs'
+              , 'convert-html'
+              , 'copy-partials'
+              , 'compress-css'
+              , 'copy-ie-js'],
+            callback);
+});
 
-gulp.task('dev', [
-  'set-dev-path'
-  ,'clean'
-  , 'copy'
-  , 'sass'
-  , 'watch'
-], function() {});
-
+gulp.task('dev', function(callback) {
+  runSequence('set-dev-path',
+            'clean',
+            ['copy'
+              , 'sass'
+              , 'watch'],
+            callback);
+});
 
 gulp.task('set-dev-path', function(){
   output = devPath;
@@ -43,15 +45,13 @@ gulp.task('set-deploy-path', function(){
   output = deployPath;
 });
 
-gulp.task('clean', ['set-dev-path'], function(cb){
-  del(output).then(function(paths){
-    cb()
-  });
+gulp.task('clean', function(){
+  return del(output);
 });
 
 gulp.task('copy', ['copy-html', 'copy-js', 'copy-lib'], function() {});
 
-gulp.task('copy-html', ['clean'], function() {
+gulp.task('copy-html', function() {
   gulp.src([
       src + 'index.html'
       , src + 'partials/**.html'
@@ -59,40 +59,40 @@ gulp.task('copy-html', ['clean'], function() {
     .pipe(gulp.dest(output));
 });
 
-gulp.task('copy-partials', ['clean'], function() {
+gulp.task('copy-partials', function() {
   gulp.src(src + 'partials/**.html', copyOptions)
     .pipe(minifyHTML())
     .pipe(gulp.dest(output));
 });
 
-gulp.task('copy-js', ['clean'], function() {
+gulp.task('copy-js', function() {
   gulp.src(src + 'js/**/*', copyOptions)
     .pipe(gulp.dest(output));
 });
 
-gulp.task('copy-lib', ['clean'], function() {
+gulp.task('copy-lib', function() {
   gulp.src(src + 'lib/**/*', copyOptions)
     .pipe(gulp.dest(output));
 });
 
-gulp.task('copy-ie-js', ['clean'], function() {
+gulp.task('copy-ie-js',  function() {
   gulp.src(src + 'lib/placeholders.min.js', copyOptions)
     .pipe(gulp.dest(output));
 });
 
-gulp.task('sass', ['clean'], function () {
+gulp.task('sass', function () {
   return gulp.src(src + 'sass/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest(output + 'css'));
 });
 
-gulp.task('watch', ['clean'], function () {
+gulp.task('watch', function () {
   gulp.watch(src + 'sass/**/*.scss', ['sass']);
   gulp.watch(src + 'js/**', ['copy-js']);
   gulp.watch(src + '**/*.html', ['copy-html']);
 });
 
-gulp.task('rjs', ['clean'], function() {
+gulp.task('rjs', function() {
     rjs({
         name: 'main',
         baseUrl: src + 'js/',
@@ -104,7 +104,7 @@ gulp.task('rjs', ['clean'], function() {
     .pipe(gulp.dest(output)); // pipe it to the output DIR
 });
 
-gulp.task('convert-html', ['clean'], function() {
+gulp.task('convert-html', function() {
   gulp.src(src + 'index.html')
     .pipe(htmlreplace({
       'app-js': 'app.js'
@@ -113,7 +113,7 @@ gulp.task('convert-html', ['clean'], function() {
     .pipe(gulp.dest(output));
 });
 
-gulp.task('compress-css', ['clean', 'sass'], function() {
+gulp.task('compress-css', ['sass'], function() {
   gulp.src(output + 'css/*.*')
     .pipe(minifyCss({advanced:false}))
     .pipe(gulp.dest(output + 'css/'));
